94933ff00b892f15508c8247b020e0249d020cd8
shramrahul <shreeramchaulagain@gmail.com>
2018-02-03 11:36:20 -0600

    required css, js and search section added

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavComponent } from './components/nav/nav.component';
+import { SearchSectionComponent } from './components/search-section/search-section.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    NavComponent
+    NavComponent,
+    SearchSectionComponent
   ],
   imports: [
     BrowserModule
