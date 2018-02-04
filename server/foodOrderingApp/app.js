b13682fbab7125bd3e93665dd29b1bccfc7ecbd1
Suman <suman.khatiwada@hotmail.com>
2018-02-03 23:33:18 -0600

    login/register with mongodb

--- server/foodOrderingApp/app.js
+++ server/foodOrderingApp/app.js
@@ -1,47 +1,62 @@
 var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-
-var index = require('./routes/index');
-var users = require('./routes/users');
-
 var app = express();
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+//connect to MongoDB
+mongoose.connect('mongodb://localhost/foodOrderingApp');
+var db = mongoose.connection;
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
+//handle mongo error
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    // we're connected!
+});
+
+//use sessions for tracking logins
+app.use(session({
+    secret: 'work',
+    resave: true,
+    saveUninitialized: false,
+    store: new MongoStore({
+        mongooseConnection: db
+    })
+}));
+
+// parse incoming requests
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', index);
-app.use('/users', users);
+
+// serve static files from template
+app.use(express.static(__dirname + '/public'));
+
+// include routes
+var routes = require('./routes/router');
+app.use('/',routes);
+app.use('/login', routes);
+app.use('/register',routes);
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
-  var err = new Error('Not Found');
-  err.status = 404;
-  next(err);
+app.use(function (req, res, next) {
+    var err = new Error('File Not Found');
+    err.status = 404;
+    next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+// define as the last app.use callback
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    console.log(err.message);
+    res.send(err.message);
 });
 
-app.listen(8080);
-module.exports = app;
+
+// listen on port 3000
+app.listen(8080, function () {
+    console.log('Express app listening on port 8080');
+});
