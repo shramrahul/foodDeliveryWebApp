10c611bc913b99f86a69e7b81aab70a9309d3285
shramrahul <shreeramchaulagain@gmail.com>
2018-02-05 10:01:10 -0600

    nav bar changed with dashboard ad restaurant-home links

--- client/src/app/app.route.ts
+++ client/src/app/app.route.ts
@@ -4,13 +4,14 @@ import {LoginComponent} from './components/login/login.component';
 import { RestaurantsSectionComponent } from './components/restaurants-section/restaurants-section.component';
 import { UserDashboardSectionComponent } from './components/user-dashboard-section/user-dashboard-section.component';
 import { Component } from '@angular/core/src/metadata/directives';
+import { RestaurantHomeSectionComponent } from './components/restaurant-home-section/restaurant-home-section.component';
 
 const MY_ROUTES: Routes = [
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
   {path:'restaurants', component:RestaurantsSectionComponent},
-  {path:'profile/:id', component: UserDashboardSectionComponent},
+  {path:'profile', component: UserDashboardSectionComponent},
   {path:'restaurants',component : RestaurantsSectionComponent},
-  {path:'restaurant/:id', component :RestaurantsSectionComponent}
+  {path:'restaurant-home', component :RestaurantHomeSectionComponent}
 ]
 export const routes = RouterModule.forRoot(MY_ROUTES);
