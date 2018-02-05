68d9cffc384a08dc09cc0ce8ebc2329a40661ce0
Suman <suman.khatiwada@hotmail.com>
2018-02-05 17:40:11 -0600

    adding user schema on mongoose

--- client/src/app/components/search-section/search-section.component.ts
+++ client/src/app/components/search-section/search-section.component.ts
@@ -10,6 +10,29 @@ export class SearchSectionComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    if (window.navigator && window.navigator.geolocation) {
+      window.navigator.geolocation.getCurrentPosition(
+        position => {
+         // this.geolocationPosition = position,
+          console.log(position.coords.latitude+""+ position.coords.longitude);
+          console.log(position)
+        },
+        error => {
+          switch (error.code) {
+            case 1:
+              console.log('Permission Denied');
+              break;
+            case 2:
+              console.log('Position Unavailable');
+              break;
+            case 3:
+              console.log('Timeout');
+              break;
+          }
+        }
+      );
+    };
+
   }
 
 }
--- server/foodOrderingApp/models/user.js
+++ server/foodOrderingApp/models/user.js
@@ -2,67 +2,91 @@ var mongoose = require('mongoose');
 var bcrypt = require('bcrypt');
 // var passport = require('passport');
 var jwt = require('jsonwebtoken');
-// var passport =require('../config/passport')(passport);
 var config = require('../config')
+var VerifyToken = require('../auth/VerifyToken');
 var UserSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true
-  },
-  username: {
-    type: String,
-    unique: true,
-    required: true,
-    trim: true
-  },
-  password: {
-    type: String,
-    required: true,
-  },
-  passwordConf: {
-    type: String,
-    required: true,
-  }
+    name: {
+        fullname: {type: String}
+    },
+    address: {
+        street: {type: String},
+        city: {type: String},
+        state: {type: String},
+        zipcode: {type: Number},
+        coord:[{latitude:{type:Number}, longitude:{type:Number}}]
+    },
+    credentials: {
+        username: {type: String, unique: true, required: true, trim: true},
+        password: {type: String, required: true},
+        email: {type: String, unique: true, required: true, trim: true}
+    },
+
+    food_ordered:[
+        {date:Date, food: {id:String, cuisine:String, name:String,price:Number},restaurant_used:Number},
+        {date:Date, food: {id:String, cuisine:String, name:String,price:Number},restaurant_used:Number},
+        {date:Date, food: {id:String, cuisine:String, name:String,price:Number},restaurant_used:Number},
+        {date:Date, food: {id:String, cuisine:String, name:String,price:Number},restaurant_used:Number},
+        ]
+    // state: {
+    //     type: String,
+    //     required: true
+    // },
+    // city: {
+    //     type: String,
+    //     required: true
+    // },
+    // street: {
+    //     type: String,
+    //     required: true
+    // },
+    // zipcode: {
+    //     type: String,
+    //     required: true
+    // }
+
+    // passwordConf: {
+    //   type: String,
+    //   required: true,
+    // }
 });
 
 
 //authenticate input against database
 UserSchema.statics.authenticate = function (email, password, callback) {
-  User.findOne({ email: email })
-    .exec(function (err, user) {
-      if (err) {
-        return callback(err)
-      } else if (!user) {
-        var err = new Error('User not found.');
-        err.status = 401;
-        return callback(err);
-      }
-      bcrypt.compare(password, user.password, function (err, result) {
-        if (result === true) {
-            var token = jwt.sign(user, config.secret, {
-                expiresIn: 10080 // in seconds
-            });
+    User.findOne({email: email})
+        .exec(function (err, user) {
+            if (err) {
+                return callback(err)
+            } else if (!user) {
+                var err = new Error('User not found.');
+                err.status = 401;
+                return callback(err);
+            }
+            bcrypt.compare(password, user.password, function (err, result) {
+                if (result === true) {
+                    // console.log("Login++++++"+user.toString());
+                    var token = jwt.sign(user, config.secret, {
+                        expiresIn: 10080 // in seconds
+                    });
 
-          return callback(null, user, token );
-        } else {
-          return callback();
-        }
-      })
-    });
+                    return callback(null, user, token);
+                } else {
+                    return callback();
+                }
+            })
+        });
 }
 
 //hashing a password before saving it to the database
 UserSchema.pre('save', function (next) {
-  var user = this;
-  bcrypt.hash(user.password, 10, function (err, hash) {
-    if (err) {
-      return next(err);
-    }
-    user.password = hash;
-    next();
-  })
+    var user = this;
+    bcrypt.hash(user.password, 10, function (err, hash) {
+        if (err) {
+            return next(err);
+        }
+        user.password = hash;
+        next();
+    })
 });
 
 
